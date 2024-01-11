a6031ec946eda325a9f4d0ddda49a24d5efed7a5
iljuhenson <ilyapavluk24@gmail.com>
2024-01-11 23:30:17 +0100

    Add README to the project

---
+++ README.md
@@ -0,0 +1,32 @@
+# How to use python???!?!?
+
+1. Stwórz nowy environment :gasp: to jest potrzebne żeby można było instalować moduły tylko dla tego projektu
+
+    `python -m venv .venv`
+
+1. Wejdź w ten environment, no kurwa python != nie rób nic, nie wszystko tutaj jest zautamatyzowane
+
+    Linuxarze gang: 
+    
+    1. `source .venv/bin/activate`
+
+    Windows usery: 
+
+    1. `.\venv\Scripts\activate`
+    1. eat shit and die loosers(no offense)
+
+1. Zainstaluj gówne które jest potrzebne żeby projekt działał 
+
+    `pip install -r requirements.txt`
+
+1. Możesz uruchomić projekt
+    
+    `python src/main.py`
+
+1. Wyjdź z environmenta
+
+    `deactivate`
+
+## PRZECZYTAJ TO BO JEST WAŻNE!!!
+
+Zawsze musisz robić krok drugi żeby ten projekt uruchomić!!!
--- src/main.py
+++ src/main.py
@@ -1,18 +1,52 @@
 import PySimpleGUI as sg
+from logic import doctor_queue_logic as logic
 
 sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+# ----------- Create the 3 layouts this Window will display -----------
+layout1 = []
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+layout2 = [
+    [sg.Text('Dodaj nowego pacjenta')],
+    [sg.Text('Imie'), sg.Input(key='-IN_NAME-')],
+    [sg.Text('Nazwisko'), sg.Input(key='-IN_SURNAME-')],
+    [sg.Text('Pesel'), sg.Input(key='-IN_PESEL-')],
+    [sg.Text('Wiek'), sg.Slider(range=(0, 100), orientation='h', key='-IN_AGE-')],
+    [sg.Checkbox('Czy jest priorytetowym?', key='-IN_IS_PRIORITY-')],
+    [sg.Button('Dodaj pacjenta'), sg.Button('Powrót do widoku głównego')],
+]
+
+layout3 = [[sg.Text('This is layout 3 - It is all Radio Buttons')],
+           *[[sg.R(f'Radio {i}', 1)] for i in range(8)]]
+
+# ----------- Create actual layout using Columns and a row of Buttons
+layout = [[sg.Column(layout1, key='-MAIN-'), sg.Column(layout2, visible=False, key='-ADD_PATIENT-'), sg.Column(layout3, visible=False, key='-COL3-')],
+          [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
+
+window = sg.Window('Swapping the contents of a window', layout)
+
+layout = 1  # The currently visible layout
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print(event, values)
+    if event in (None, 'Exit'):
         break
-    print('You entered ', values[0])
+    if event == 'Cycle Layout':
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout + 1 if layout < 3 else 1
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event in '123':
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = int(event)
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event == 'Zatwerdź':
+        name = values['-IN_NAME-']
+        surname = values['-IN_SURNAME-']
+        pesel = values['-IN_PESEL-']
+        age = values['-IN_AGE-']
 
+
+    elif event == 'Powrót do widoku głównego':
+        window[f'-ADD_PATIENT-'].update(visible=False)
+        layout = int(event)
+        window[f'-MAIN-'].update(visible=True)
 window.close()
